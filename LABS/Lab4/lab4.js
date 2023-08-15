d1d4fe71b0daba1530e637e3b666b7cd9e4cbe9d
FaKeArtur27 <arsanmx@gmail.com>
2023-08-14 23:42:02 -0600

    Mitad del lab4

--- LABS/Lab4/lab4.js
+++ LABS/Lab4/lab4.js
@@ -2,6 +2,6 @@ alert("Hola mundo");
 
 console.error("Este es un mensaje de error");
 
-const nombre = prompt("¿Cómo te llamas?");
+const nombre = prompt("Como te llamas?");
 console.log("Hola " + nombre);
 
