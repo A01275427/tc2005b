4a5374e84b17f38002a23c61195af8c8f0c7ff2f
FaKeArtur27 <arsanmx@gmail.com>
2023-08-23 00:13:43 -0600

    Generando una mejor página para el lab11

---
+++ LABS/Lab11/lab11.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use
+
+app.use((request, response, next) => {
+    console.log('Middleware');
+    next();
+});
+
+app.get('/',(request, response, next) => {
+    const html =` 
+    <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Laboratorio 11</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="lab11.css"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <header>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28">
+                </a>
+            </div>
+        </nav>
+        <section class="section">
+            <div class="container">
+                <h1 class="title">Laboratorio 11: Express</h1>
+                <p class="subtitle">Arturo Sánchez Rodríguez - A01275427</p>
+            </div>
+        </section>
+    </header>
+    <main>
+        <section>
+            <div class="container">
+                <h1 class="title"><strong>Aprendizajes</strong></h1>
+                <ul>
+                    <li><button class="button">HTML</button></li>
+                    <li><button class="button">JavaScript</button></li>
+                    <li><button class="button">Git/Terminal</button></li>
+                </ul>
+
+                <div id="DatosPersonales"></div>
+
+                <table>
+                    <caption>Datos Personales</caption>
+                    <thead>
+                        <tr>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Fecha Nacimiento</th>
+                            <th scope="col">Edad</th>
+                            <th scope="col">Carrera</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Arturo</td>
+                            <td>27/08/2002</td>
+                            <td>20</td>
+                            <td>ITC</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
+`
+
+response.send(html);
+});
+
+app.post('/new', (request, response, next) => {
+    console.log(request.body);
+    response.send();
+})
+
+app.use((request, response, next) => {
+    response.statusCode = 404;
+    response.send('No se puede Krnal');
+});
+
+app.listen(3000);
