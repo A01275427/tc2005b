10e666cc6828c12e1d9defcd7950b804b39d3b1b
FaKeArtur27 <arsanmx@gmail.com>
2023-09-03 21:30:33 -0600

    Lab11 creo que ya esta, falta solucionar error con npm start

--- LABS/Lab11/lab11.js
+++ LABS/Lab11/lab11.js
@@ -3,132 +3,192 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-app.use
+app.use(bodyParser.urlencoded({extended: false}));
+let equipo = [
+    {
+        agarre:"Anatomico",
+        imagen:"https://http2.mlstatic.com/D_NQ_NP_958731-MLA48578386701_122021-O.webp",
+        descripcion:"Se adaptan a la mano en forma por por lo que están aconsejados para personas con manos grandes a las que les gusta sentir que usan toda la superficie de la pala."
+    },
+    {
+        agarre:"Recto",
+        imagen:"https://http2.mlstatic.com/D_Q_NP_655478-MLA48578011679_122021-O.webp",
+        descripcion:"Son más anchos por el extremo del mango y más estrechos en la conexión con la cabeza de la pala. Una forma diseñada para evitar que la pala se resbale de las manos. En este tipo existen algunos modelos que varían la anchura del mango, por lo que se puede conseguir un agarre casi perfecto."
+    },
+    {
+        agarre:"Concavo",
+        imagen:"https://http2.mlstatic.com/D_NQ_NP_953046-CBT68938758100_042023-O.webp",
+        descripcion:"Se adaptan a la mano en forma por por lo que están aconsejados para personas con manos grandes a las que les gusta sentir que usan toda la superficie de la pala."
+    }
+]
+equipo.push({
+    agarre: request.body.agarre,
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_958731-MLA48578386701_122021-O.webp",
+    descripcion: request.body.descripcion,
+})
 
 app.use((request, response, next) => {
     console.log('Middleware');
-    next();
+    next();//Le permite a la petición avanzar hacia el siguiente middleware
 });
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
 
 app.get('/',(request, response, next) => {
     const html =` 
     <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
     <meta charset="utf-8">
-    <title>Laboratorio 11</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-    <link rel="stylesheet" href="lab11.css"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laboratorios</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="lab6.css">
 </head>
 <body>
     <header>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
+        <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28">
+                <a class="navbar-item" href="#">
+                    <img src="https://cdn.pixabay.com/photo/2022/05/23/16/05/table-tennis-7216580_1280.png" alt="Tenis de Mesa" width="20" height="20">
                 </a>
+                <section class="section">
+                    <div class="container">
+                        <h1 class="title is-4">Tenis de Mesa</h1>
+                    </div>
+                </section>
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+                </div>
+                <div class="tile">
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                    Inicio
+                    </a>
+            
+                    <a class="navbar-item">
+                    Historia
+                    </a>
+            
+                    <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        Mas
+                    </a>
+            
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                        Ranking
+                        </a>
+                        <a class="navbar-item">
+                        Arbitraje
+                        </a>
+                        <a class="navbar-item">
+                        Contact
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                        Soporte
+                        </a>
             </div>
         </nav>
-        <section class="section">
-            <div class="container">
-                <h1 class="title">Laboratorio 11: Express</h1>
-                <p class="subtitle">Arturo Sánchez Rodríguez - A01275427</p>
-            </div>
-        </section>
+        
     </header>
     <main>
-        <section>
+        <section class="section">
             <div class="container">
-                <h1 class="title"><strong>Aprendizajes</strong></h1>
-                <ul>
-                    <li><button class="button">HTML</button></li>
-                    <li><button class="button">JavaScript</button></li>
-                    <li><button class="button">Git/Terminal</button></li>
+                <h2 class="title is-4">Informacion General</h2>
+                <ul class="buttons">
+                    <li><button class="button is-transparent">Torneos</button></li>
+                    <li><button class="button is-transparent">Categoria</button></li>
+                    <li><button class="button is-transparent">Reglas</button></li>
+                    <li><button class="button is-transparent">Equipo</button></li>
                 </ul>
+            </div>
 
-                <div id="DatosPersonales"></div>
+            <div class="columns">
+            <div class="column">
+                <figure class="image">
+                    <img class="is-rounded" src="https://http2.mlstatic.com/D_NQ_NP_958731-MLA48578386701_122021-O.webp">
+                </figure>
+            </div>
+            <div class="column">
+                <figure class="image">
+                    <img class="is-rounded" src="https://http2.mlstatic.com/D_Q_NP_655478-MLA48578011679_122021-O.webp">
+                </figure>
+            </div>
+            <div class="column">
+                <figure class="image">
+                    <img class="is-rounded" src="https://http2.mlstatic.com/D_NQ_NP_953046-CBT68938758100_042023-O.webp">
+                </figure>
+            </div>
+            <button id="boton_imagen" class="button is-info is-rounded">Info Equipo</button>
 
-                <table>
-                    <caption>Datos Personales</caption>
+
+            <div id="equipo"></div>
+        </section>
+        <section id="cajaNombre">
+        <div>
+            <form class="box is-transparent">
+            <div class="field">
+                <label for="Nombre">Nombre</label>
+                <div class="control">
+                <input id="Nombre" class="input" type="text" placeholder="Usuario-123">
+                </div>
+            </div>
+        </div>
+        </section>
+        <section class="section">
+            <div class="container">
+                <h3 class="title is-5">Datos de Altleta</h3>
+                <table class="table", style="background-color: transparent;">
+                    <caption></caption>
                     <thead>
                         <tr>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Fecha Nacimiento</th>
-                            <th scope="col">Edad</th>
-                            <th scope="col">Carrera</th>
+                            <th>Nombre</th>
+                            <th>Fecha Nacimiento</th>
+                            <th>Edad</th>
+                            <th>Categoria</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Arturo</td>
+                            <td>Arturo Sanchez Rodriguez</td>
                             <td>27/08/2002</td>
-                            <td>20</td>
-                            <td>ITC</td>
+                            <td>21</td>
+                            <td>Sub-21</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+            <br>
+            <framset>
+                <div class="container">
+                    <h5 id="title">
+                        HolaHolaaaa
+                    </h5>
+                </div>
+            </framset>
         </section>
     </main>
+    <footer class="section">
+        <div class="container">
+            <h4 class="title is-5">Referencias</h4>
+        </div>
+    </footer>
+    <script src="lab6.js"></script>
 </body>
 </html>
-`
-
-response.send(html);
-});
-
-app.use((request, response, next) => {
-    console.log('Middleware!');
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
-});
-
-app.get('/peliculas', (request, response, next) => {
-
-    const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="utf-8">
-    <title>Películas</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-    <header>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-    <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28">
-    </a>
-    
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-    <span aria-hidden="true"></span>
-    <span aria-hidden="true"></span>
-    <span aria-hidden="true"></span>
-    </a>
-    </div>
-    </nav>
-    </header>
-    <main>
-    <section class="section">
-    <div class="container">
-    <h1 class="title">Registro de películas</h1>
-    <form action="/new" method="POST">
-    <label for="nombre">Nombre de la película</label>
-    <input id="nombre" name="nombre" class="input" type="text" placeholder="Oppenheimer">
-    <br><br>
-    <label for="sinopsis">Sinapsis de la película</label>
-    <textarea id="sinopsis" name="sinopsis" class="textarea" placeholder="La historia de la bomba atómica"></textarea>
-    <br>
-    <input id="registrar" name="registrar" type="submit" value="Registrar" class="button is-info">
-    </form>
-    </div>
-    </section>
-    </main>
-    </body>
-    </html>
-    `;
+`;
     
     response.send(html);
 });
@@ -182,9 +242,19 @@ app.get('/ola', (request, response, next) => {
     response.send();
 })
 
+app.post('/new', (request, response, next) => {
+
+    console.log(request.body);
+
+    response.redirect('/');
+
+});
+
 app.use((request, response, next) => {
+    console.log('Otro middleware!');
     response.statusCode = 404;
-    response.send('No se puede Krnal');
+    response.send('Es película es muuuy independiente'); //Manda la respuesta
+
 });
 
 app.listen(3000);
