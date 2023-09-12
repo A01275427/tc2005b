35e618f56ad1fb632b9d8e936963fe45675356da
FaKeArtur27 <arsanmx@gmail.com>
2023-09-12 14:33:22 -0600

    Falla

--- Lab/lab.js
+++ Lab/lab.js
@@ -1,6 +1,12 @@
 const express = require('express');
+
 const app = express();
+
 const bodyParser = require('body-parser');
+
+//const router = require('./router'); 
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((request, response, next) => {
@@ -15,15 +21,16 @@ app.post((request, response, next) => {
 
 });
 
-const rutasMotos = require('./routes/lab.routes');
 
-app.use('/Lab', rutasMotos);
+const rutamoto = require('../routes/lab.routesa');
+
+//app.use('/enduro', rutamoto);
+
+
 
 app.use((request, response, next) => {
-
     console.log('Otro middleware!');
+    response.statusCode = 404;
+    response.send('Es película es muuuy independiente'); //Manda la respuesta
 
-})
-
-
-app.listen(3000);
+});
